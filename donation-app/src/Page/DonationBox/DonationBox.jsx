2fe363a0f4698ac8a9194a319cd29daec05e871f
Max Plaga <johndoe@example.com>
2019-09-28 19:20:19 +0200

    added input for donation amount

--- donation-app/src/Page/DonationBox/DonationBox.jsx
+++ donation-app/src/Page/DonationBox/DonationBox.jsx
@@ -4,22 +4,53 @@ export default class DonationBox extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: new Date()
+            totalNeeded: 1000,
+            totalReached: 0,
+            donationsCount: 43,
+            amountToDonate: 50,
+            value: 0,
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    componentDidMount() {
+        this.setState({
+            value: this.state.amountToDonate,
+            totalReached: 300,
+        });
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        this.setState({
+            totalReached: this.state.totalReached + this.state.value,
+        });
+        event.preventDefault();
+    };
 
     render() {
-
+        const totalLeft = this.state.totalNeeded - this.state.totalReached;
         return (
             <div>
                 <h3>Donation</h3>
 
                 <div className="card">
-                    <p>Join the {}</p>
+                    <p>Amount donated so far: ${this.state.totalReached}</p>
+                    <p>Needed amount left: ${totalLeft}</p>
+                    <p>Add to the <b>{this.state.donationsCount}</b>   other donations that kind people like yourself already contributed.</p>
 
-                    <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-                    <button type="button" className="da-success">Give Now</button>
+                        <form id="da-form-donation" onSubmit={this.handleSubmit}>
+                            <label>
+                                $
+                                <input type="number" value={this.state.value} onChange={this.handleChange} />
+                            </label>
+                            <button type="button" className="da-success" onClick={this.handleSubmit}>Give Now</button>
+                        </form>
                 </div>
                 <button type="button">Save for later</button>
                 <button type="button">Tell your friends</button>
