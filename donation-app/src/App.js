e28a6e8b3ecfae82969862f510d668fc983377e9
Max Plaga <max.plaga@student.hpi.de>
2019-09-29 14:22:47 +0200

    added new files

---
+++ donation-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Page from './Page/Page';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <Page/>
+        </div>
+    );
+}
+
+export default App;
